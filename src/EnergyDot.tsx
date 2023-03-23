2759a8bc8cd75e34158bc9fa746de777ab0084b6
Manas Purohit <purohit.m@husky.neu.edu>
2023-03-23 16:14:23 -0400

    add EnergyDot component

---
+++ src/EnergyDot.tsx
@@ -0,0 +1,32 @@
+interface Props {
+  energy: number
+}
+
+function EnergyDot({ energy }: Props) {
+  let color = '';
+
+  // assumption: energy is a non-negative number
+  if (energy >= 0 && energy < 20) { // assumption: 0-20 --> [0, 20)
+    color = 'red';
+  } else if (energy >= 20 && energy < 100) { // assumption: 20-100 --> [20, 100)
+    color = 'orange';
+  } else if (energy >= 100) { // assumption: 100+ --> [100, infinity)
+    color = 'green';
+  }
+
+  const circleStyle =  {
+    display: 'flex',
+    width: '10px',
+    height: '10px',
+    borderWidth: '6px',
+    borderStyle: 'solid',
+    borderColor: color,
+    borderRadius: '50%',
+  };
+
+  return (
+    <div style={circleStyle}></div>
+  );
+}
+
+export default EnergyDot;
