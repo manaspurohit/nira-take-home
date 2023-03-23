ee60dfaf0f176034587fc6bc97d4362a700d2cbb
Manas Purohit <purohit.m@husky.neu.edu>
2023-03-23 16:18:03 -0400

    updated App to parse data and show BusDetail

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import BusDetail from './BusDetail';
 import './App.css';
 
+const data = {
+  bus_id: "25518",
+  voltage: 138,
+  owner: "Northern Indiana Public Service Co LLC",
+  latitude: 41.494,
+  longitude: -87.026,
+  bus_name: "Flint Lake - 138 kV",
+  mw_energy: 70,
+  mw_capacity: 70,
+  allocated_cost: 9780000,
+  total_cost: 63720000,
+  number_constraints: 4,
+};
+
 function App() {
+  const {
+    bus_id: busId,
+    voltage,
+    owner,
+    latitude,
+    longitude,
+    bus_name: busName,
+    mw_energy: mwEnergy,
+    mw_capacity: mwCapacity,
+    allocated_cost: allocatedCost,
+    total_cost: totalCost,
+    number_constraints: numberConstraints
+  } = data;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <BusDetail 
+        busId={busId} 
+        voltage={voltage} 
+        owner={owner} 
+        latitude={latitude} 
+        longitude={longitude} 
+        busName={busName} 
+        mwEnergy={mwEnergy}
+        mwCapacity={mwCapacity}
+        allocatedCost={allocatedCost}
+        totalCost={totalCost}
+        numberConstraints={numberConstraints}
+      />
     </div>
   );
 }
